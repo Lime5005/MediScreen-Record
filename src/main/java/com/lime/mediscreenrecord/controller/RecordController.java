d7d8cafe5e5352b3b2530ba4fdd5235b0d1ce9d5
Liping Mechling <65612959+Lime5005@users.noreply.github.com>
2022-02-08 18:44:55 +0100

    Add swagger @Api and @ApiOperation

--- src/main/java/com/lime/mediscreenrecord/controller/RecordController.java
+++ src/main/java/com/lime/mediscreenrecord/controller/RecordController.java
@@ -2,6 +2,8 @@ package com.lime.mediscreenrecord.controller;
 
 import com.lime.mediscreenrecord.model.Record;
 import com.lime.mediscreenrecord.service.RecordService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
+@Api(tags = "Record's data API")
 @RequestMapping("/api")
 public class RecordController {
     Logger logger = LoggerFactory.getLogger(RecordController.class);
@@ -26,11 +29,13 @@ public class RecordController {
     }
 
     @GetMapping("/records")
+    @ApiOperation("Get all Records from database")
     public List<Record> getAllRecords() {
         return recordService.findAll();
     }
 
     @GetMapping("/records/patient/{patientId}")
+    @ApiOperation("Get all Records by one Patient's ID")
     public ResponseEntity<List<Record>> getOnePatientRecords(@PathVariable(value = "patientId") Long patientId) {
         List<Record> recordList = recordService.findByPatientId(patientId);
         if (recordList.isEmpty()) {
@@ -42,6 +47,7 @@ public class RecordController {
     }
 
     @GetMapping("/records/{id}")
+    @ApiOperation("Find one Record by record's ID")
     public ResponseEntity<?> findRecordById(@PathVariable(value = "id") Long id) {
         Record record = recordService.findById(id);
         if (record == null) {
@@ -53,6 +59,7 @@ public class RecordController {
     }
 
     @PostMapping("/records/patient/{patientId}/add")
+    @ApiOperation("Create a new Record by one patient's ID")
     public ResponseEntity<?> createRecord(@PathVariable(value = "patientId") Long patientId, @Valid @RequestBody Record record) {
         Record newRecord = recordService.createRecord(patientId, record);
         if (newRecord == null) {
@@ -64,6 +71,7 @@ public class RecordController {
     }
 
     @PutMapping("/records/update/{id}")
+    @ApiOperation("Update a Record by record's ID")
     public ResponseEntity<?> updateRecord(@PathVariable(value = "id") Long id, @Valid @RequestBody Record recordDetails) {
         Record record = recordService.updateRecord(id, recordDetails);
         if (record == null) {
@@ -75,6 +83,7 @@ public class RecordController {
     }
 
     @DeleteMapping("/records/{id}")
+    @ApiOperation("Delete a Record by record's ID")
     public ResponseEntity<?> deleteRecord(@PathVariable(value = "id") Long id) {
         Boolean deleted = recordService.deleteRecordById(id);
         if (!deleted) {
